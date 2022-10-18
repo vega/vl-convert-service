ce3369fa001d4ac890274ee97dff2553ede72df5
Jon Mease <jonmmease@gmail.com>
2022-10-17 19:56:28 -0500

    Try some validation

--- api/vl2vg.py
+++ api/vl2vg.py
@@ -8,13 +8,25 @@ class handler(BaseHTTPRequestHandler):
         query_params = dict(parse.parse_qsl(parse.urlsplit(s).query))
 
         # Extract query params
-        vl_spec = query_params.get("vl_spec")
-        vl_version = query_params.get("vl_version", None)
+        if "vl_spec" not in query_params:
+            self.send_response(400)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write("vl_spec query parameter required".encode())
+        else:
+            vl_spec = query_params.get("vl_spec")
+            vl_version = query_params.get("vl_version", None)
 
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
+            try:
+                vg_spec = vlc.vegalite_to_vega(vl_spec, vl_version=vl_version)
+                self.send_response(200)
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+                self.wfile.write(vg_spec.encode())
+            except Exception as e:
+                self.send_response(400)
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+                self.wfile.write(f"conversion failed: {str(e)}".encode())
 
-        vg_spec = vlc.vegalite_to_vega(vl_spec, vl_version=vl_version)
-        self.wfile.write(vg_spec.encode())
         return
