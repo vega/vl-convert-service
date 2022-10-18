5bf270dc62455f180ebe5d0426266d577cf30d3d
Jon Mease <jonmmease@gmail.com>
2022-10-18 09:21:44 -0500

    Add vl2pdf

---
+++ api/vl2pdf.py
@@ -0,0 +1,63 @@
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+import vl_convert as vlc
+from svglib import svglib
+from reportlab.graphics import renderPDF
+from tempfile import TemporaryDirectory
+from os.path import join
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        s = self.path
+        query_params = dict(parse.parse_qsl(parse.urlsplit(s).query))
+
+        # Extract query params
+        if "vl_spec" not in query_params:
+            self.send_response(400)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write("vl_spec query parameter required".encode())
+        else:
+            vl_spec = query_params.get("vl_spec")
+            self.convert(vl_spec, query_params)
+
+    def do_POST(self):
+        s = self.path
+        query_params = dict(parse.parse_qsl(parse.urlsplit(s).query))
+        content_len = int(self.headers.get('Content-Length', 0))
+        if content_len == 0:
+            self.send_response(400)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write("POST body must be vl_spec".encode())
+        else:
+            vl_spec = self.rfile.read(content_len).decode("utf8")
+            self.convert(vl_spec, query_params)
+
+    def convert(self, vl_spec, query_params):
+        vl_version = query_params.get("vl_version", None)
+        try:
+            svg = vlc.vegalite_to_svg(vl_spec, vl_version=vl_version)
+
+            with TemporaryDirectory() as path:
+                svg_path = join(path, "chart.svg")
+                pdf_path = join(path, "chart.pdf")
+
+                with open(svg_path, "wt") as f:
+                    f.write(svg)
+
+                drawing = svglib.svg2rlg("file.svg")
+                renderPDF.drawToFile(drawing, pdf_path)
+
+                with open(pdf_path, "rb") as f:
+                    pdf_bytes = f.read()
+
+            self.send_response(200)
+            self.send_header('Content-type', 'application/pdf')
+            self.end_headers()
+            self.wfile.write(pdf_bytes)
+        except Exception as e:
+            self.send_response(400)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(f"conversion failed: {str(e)}".encode())
