88932f6f30597edf7beb9a763fbbdd2dccfe6655
Jon Mease <jonmmease@gmail.com>
2022-10-17 19:49:45 -0500

    Add vl2vg query params

--- api/vl2vg.py
+++ api/vl2vg.py
@@ -1,31 +1,20 @@
 from http.server import BaseHTTPRequestHandler
+from urllib import parse
 import vl_convert as vlc
 
 class handler(BaseHTTPRequestHandler):
-
     def do_GET(self):
+        s = self.path
+        query_params = dict(parse.parse_qsl(parse.urlsplit(s).query))
+
+        # Extract query params
+        vl_spec = query_params.get("vl_spec")
+        vl_version = query_params.get("vl_version", None)
+
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header('Content-type', 'text/plain')
         self.end_headers()
 
-        vl_spec = r"""
-{
-  "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
-  "data": {"url": "https://raw.githubusercontent.com/vega/vega-datasets/next/data/movies.json"},
-  "mark": "circle",
-  "encoding": {
-    "x": {
-      "bin": {"maxbins": 10},
-      "field": "IMDB Rating"
-    },
-    "y": {
-      "bin": {"maxbins": 10},
-      "field": "Rotten Tomatoes Rating"
-    },
-    "size": {"aggregate": "count"}
-  }
-}
-        """
-        vg_spec = vlc.vegalite_to_vega(vl_spec)
+        vg_spec = vlc.vegalite_to_vega(vl_spec, vl_version=vl_version)
         self.wfile.write(vg_spec.encode())
         return
