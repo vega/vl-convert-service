fc5bc25ce65da9660a3a0e1b4c8880b5860d36af
Jon Mease <jonmmease@gmail.com>
2022-10-17 20:01:52 -0500

    Add vl2svg

--- api/vl2svg.py
+++ api/vl2svg.py
@@ -18,11 +18,11 @@ class handler(BaseHTTPRequestHandler):
             vl_version = query_params.get("vl_version", None)
 
             try:
-                vg_spec = vlc.vegalite_to_vega(vl_spec, vl_version=vl_version)
+                svg = vlc.vegalite_to_svg(vl_spec, vl_version=vl_version)
                 self.send_response(200)
-                self.send_header('Content-type', 'text/json')
+                self.send_header('Content-type', 'text/svg')
                 self.end_headers()
-                self.wfile.write(vg_spec.encode())
+                self.wfile.write(svg.encode())
             except Exception as e:
                 self.send_response(400)
                 self.send_header('Content-type', 'text/plain')
