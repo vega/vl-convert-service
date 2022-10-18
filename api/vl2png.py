57fe6c5e315a00cb118eedbf95a3423e02d115af
Jon Mease <jonmmease@gmail.com>
2022-10-18 07:25:26 -0500

    POST for vl2png and vl2svg

--- api/vl2png.py
+++ api/vl2png.py
@@ -15,23 +15,37 @@ class handler(BaseHTTPRequestHandler):
             self.wfile.write("vl_spec query parameter required".encode())
         else:
             vl_spec = query_params.get("vl_spec")
-            vl_version = query_params.get("vl_version", None)
-            scale = query_params.get("scale", None)
+            self.convert(vl_spec, query_params)
 
-            try:
-                png_data = vlc.vegalite_to_png(
-                    vl_spec,
-                    vl_version=vl_version,
-                    scale=float(scale) if scale is not None else None
-                )
-                self.send_response(200)
-                self.send_header('Content-type', 'image/png')
-                self.end_headers()
-                self.wfile.write(png_data)
-            except Exception as e:
-                self.send_response(400)
-                self.send_header('Content-type', 'text/plain')
-                self.end_headers()
-                self.wfile.write(f"conversion failed: {str(e)}".encode())
+    def to_POST(self):
+        s = self.path
+        query_params = dict(parse.parse_qsl(parse.urlsplit(s).query))
+        content_len = int(self.headers.get('Content-Length', 0))
+        if content_len == 0:
+            self.send_response(400)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write("POST body must be vl_spec".encode())
+        else:
+            vl_spec = self.rfile.read(content_len).decode("utf8")
+            self.convert(vl_spec, query_params)
 
-        return
+    def convert(self, vl_spec, query_params):
+        vl_version = query_params.get("vl_version", None)
+        scale = query_params.get("scale", None)
+
+        try:
+            png_data = vlc.vegalite_to_png(
+                vl_spec,
+                vl_version=vl_version,
+                scale=float(scale) if scale is not None else None
+            )
+            self.send_response(200)
+            self.send_header('Content-type', 'image/png')
+            self.end_headers()
+            self.wfile.write(png_data)
+        except Exception as e:
+            self.send_response(400)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(f"conversion failed: {str(e)}".encode())
--- api/vl2svg.py
+++ api/vl2svg.py
@@ -15,18 +15,32 @@ class handler(BaseHTTPRequestHandler):
             self.wfile.write("vl_spec query parameter required".encode())
         else:
             vl_spec = query_params.get("vl_spec")
-            vl_version = query_params.get("vl_version", None)
+            self.convert(vl_spec, query_params)
 
-            try:
-                svg = vlc.vegalite_to_svg(vl_spec, vl_version=vl_version)
-                self.send_response(200)
-                self.send_header('Content-type', 'image/svg+xml')
-                self.end_headers()
-                self.wfile.write(svg.encode())
-            except Exception as e:
-                self.send_response(400)
-                self.send_header('Content-type', 'text/plain')
-                self.end_headers()
-                self.wfile.write(f"conversion failed: {str(e)}".encode())
+    def to_POST(self):
+        s = self.path
+        query_params = dict(parse.parse_qsl(parse.urlsplit(s).query))
+        content_len = int(self.headers.get('Content-Length', 0))
+        if content_len == 0:
+            self.send_response(400)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write("POST body must be vl_spec".encode())
+        else:
+            vl_spec = self.rfile.read(content_len).decode("utf8")
+            self.convert(vl_spec, query_params)
 
-        return
+    def convert(self, vl_spec, query_params):
+        vl_version = query_params.get("vl_version", None)
+
+        try:
+            svg = vlc.vegalite_to_svg(vl_spec, vl_version=vl_version)
+            self.send_response(200)
+            self.send_header('Content-type', 'image/svg+xml')
+            self.end_headers()
+            self.wfile.write(svg.encode())
+        except Exception as e:
+            self.send_response(400)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(f"conversion failed: {str(e)}".encode())
