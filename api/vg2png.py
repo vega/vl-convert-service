46f0e0928bf0dd7be15b9bba88b7e965f2a60965
Jon Mease <jonmmease@gmail.com>
2023-10-29 07:14:10 -0400

    Add theme and ppi query params

--- api/vg2png.py
+++ api/vg2png.py
@@ -9,11 +9,13 @@ vlc.register_font_directory(str(Path("fonts").absolute()))
 class handler(VgHandler):
     def convert_vg(self, vg_spec, query_params):
         scale = query_params.get("scale", None)
+        ppi = query_params.get("ppi", None)
 
         try:
             png_data = vlc.vega_to_png(
                 vg_spec,
                 scale=float(scale) if scale is not None else None,
+                ppi=float(ppi) if ppi is not None else None,
                 allowed_base_urls=ALLOWED_BASE_URLS
             )
             self.send_successful(png_data, 'image/png')
--- api/vl2svg.py
+++ api/vl2svg.py
@@ -9,11 +9,12 @@ vlc.register_font_directory(str(Path("fonts").absolute()))
 class handler(VlHandler):
     def convert_vl(self, vl_spec, query_params):
         vl_version = query_params.get("vl_version", None)
-
+        theme = query_params.get("theme", None)
         try:
             svg = vlc.vegalite_to_svg(
                 vl_spec,
                 vl_version=vl_version,
+                theme=theme,
                 allowed_base_urls=ALLOWED_BASE_URLS,
             )
             self.send_successful(svg.encode(), 'image/svg+xml')
