50a427dfec8cc37d758f5005f3896a3a742262bd
Jon Mease <jonmmease@gmail.com>
2023-10-30 07:20:29 -0400

    rename to APP_URL

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,7 +3,8 @@ import vl_convert as vlc
 import pytest
 import json
 
-DEV_URL = " http://localhost:3000"
+APP_URL = "http://localhost:3000"
+# APP_URL = "http://vl-convert-service.vercel.app"
 
 
 @pytest.fixture
@@ -38,7 +39,7 @@ def vl_spec_invalid_base_url():
 
 # Version test
 def test_version():
-    version_url = f"{DEV_URL}/api/version"
+    version_url = f"{APP_URL}/api/version"
     response = requests.get(version_url)
     assert response.status_code == 200
     assert vlc.__version__ == response.content.decode()
@@ -46,7 +47,7 @@ def test_version():
 
 # SVG tests
 def test_vl2svg(vl_spec):
-    url = f"{DEV_URL}/api/vl2svg"
+    url = f"{APP_URL}/api/vl2svg"
     response = requests.post(url, json=vl_spec)
     assert response.status_code == 200
     assert response.headers["Content-type"] == "image/svg+xml"
@@ -55,14 +56,14 @@ def test_vl2svg(vl_spec):
 
 
 def test_vg2svg(vl_spec):
-    url = f"{DEV_URL}/api/vl2vg"
+    url = f"{APP_URL}/api/vl2vg"
     response = requests.post(url, json=vl_spec)
     assert response.status_code == 200
     assert response.headers["Content-type"] == "application/json"
     vg_spec = json.loads(response.content.decode("utf8"))
     assert vg_spec["$schema"] == "https://vega.github.io/schema/vega/v5.json"
 
-    url = f"{DEV_URL}/api/vg2svg"
+    url = f"{APP_URL}/api/vg2svg"
     response = requests.post(url, json=vg_spec)
     assert response.status_code == 200
     assert response.headers["Content-type"] == "image/svg+xml"
@@ -72,7 +73,7 @@ def test_vg2svg(vl_spec):
 
 def test_vl2svg_allowed_base_url(vl_spec_invalid_base_url):
     # Loading data from GitHub is forbidden by base url rules
-    url = f"{DEV_URL}/api/vl2svg"
+    url = f"{APP_URL}/api/vl2svg"
     response = requests.post(url, json=vl_spec_invalid_base_url)
     assert response.status_code == 400
     assert response.headers["Content-type"] == "text/plain"
@@ -82,7 +83,7 @@ def test_vl2svg_allowed_base_url(vl_spec_invalid_base_url):
 
 # PNG tests
 def test_vl2png(vl_spec):
-    url = f"{DEV_URL}/api/vl2png?scale=2"
+    url = f"{APP_URL}/api/vl2png?scale=2"
     response = requests.post(url, json=vl_spec)
     assert response.status_code == 200
     assert response.headers["Content-type"] == "image/png"
@@ -91,14 +92,14 @@ def test_vl2png(vl_spec):
 
 
 def test_vg2png(vl_spec):
-    url = f"{DEV_URL}/api/vl2vg"
+    url = f"{APP_URL}/api/vl2vg"
     response = requests.post(url, json=vl_spec)
     assert response.status_code == 200
     assert response.headers["Content-type"] == "application/json"
     vg_spec = json.loads(response.content.decode("utf8"))
     assert vg_spec["$schema"] == "https://vega.github.io/schema/vega/v5.json"
 
-    url = f"{DEV_URL}/api/vg2png?scale=2"
+    url = f"{APP_URL}/api/vg2png?scale=2"
     response = requests.post(url, json=vg_spec)
     assert response.status_code == 200
     assert response.headers["Content-type"] == "image/png"
@@ -108,7 +109,7 @@ def test_vg2png(vl_spec):
 
 def test_vl2png_allowed_base_url(vl_spec_invalid_base_url):
     # Loading data from GitHub is forbidden by base url rules
-    url = f"{DEV_URL}/api/vl2png"
+    url = f"{APP_URL}/api/vl2png"
     response = requests.post(url, json=vl_spec_invalid_base_url)
     assert response.status_code == 400
     assert response.headers["Content-type"] == "text/plain"
@@ -118,7 +119,7 @@ def test_vl2png_allowed_base_url(vl_spec_invalid_base_url):
 
 # PDF tests
 def test_vl2pdf(vl_spec):
-    url = f"{DEV_URL}/api/vl2pdf?scale=2&theme=dark"
+    url = f"{APP_URL}/api/vl2pdf?scale=2&theme=dark"
     response = requests.post(url, json=vl_spec)
     assert response.status_code == 200
     assert response.headers["Content-type"] == "application/pdf"
@@ -127,14 +128,14 @@ def test_vl2pdf(vl_spec):
 
 
 def test_vg2pdf(vl_spec):
-    url = f"{DEV_URL}/api/vl2vg"
+    url = f"{APP_URL}/api/vl2vg"
     response = requests.post(url, json=vl_spec)
     assert response.status_code == 200
     assert response.headers["Content-type"] == "application/json"
     vg_spec = json.loads(response.content.decode("utf8"))
     assert vg_spec["$schema"] == "https://vega.github.io/schema/vega/v5.json"
 
-    url = f"{DEV_URL}/api/vg2pdf?scale=2&theme=dark"
+    url = f"{APP_URL}/api/vg2pdf?scale=2&theme=dark"
     response = requests.post(url, json=vg_spec)
     assert response.status_code == 200
     assert response.headers["Content-type"] == "application/pdf"
@@ -144,7 +145,7 @@ def test_vg2pdf(vl_spec):
 
 def test_vl2pdf_allowed_base_url(vl_spec_invalid_base_url):
     # Loading data from GitHub is forbidden by base url rules
-    url = f"{DEV_URL}/api/vl2pdf"
+    url = f"{APP_URL}/api/vl2pdf"
     response = requests.post(url, json=vl_spec_invalid_base_url)
     assert response.status_code == 400
     assert response.headers["Content-type"] == "text/plain"
